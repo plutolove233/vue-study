53f219cccfddd78a09e17646fb5b28673dc05ddc
yizhigopher <yizhigopher@foxmail.com>
2023-01-19 22:25:01 +0800

    todolist version--1

---
+++ todo-list/src/components/ToDoMain.vue
@@ -0,0 +1,54 @@
+<template>
+	<ul class="todo-main">
+		<div v-for="item in todo" :key="item.id">
+			<todo-item
+				:item="item"
+				@changeStatus="changeStatus"
+				@deleteTodo="deleteTodo"
+			/>
+		</div>
+	</ul>
+</template>
+
+<script>
+import TodoItem from "./ToDoItem.vue";
+export default {
+	name: "ToDoMain",
+	components: {
+		TodoItem,
+	},
+	props: ["todo"],
+	data() {
+		return {
+			msg: "",
+		};
+	},
+	methods: {
+		changeStatus(id) {
+			this.$emit("changeStatus", id);
+		},
+		deleteTodo(id) {
+			this.$emit("deleteTodo", id);
+		},
+	},
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+	margin-left: 0px;
+	border: 1px solid #ddd;
+	border-radius: 2px;
+	padding: 0px;
+}
+
+.todo-empty {
+	height: 40px;
+	line-height: 40px;
+	border: 1px solid #ddd;
+	border-radius: 2px;
+	padding-left: 5px;
+	margin-top: 10px;
+}
+</style>
